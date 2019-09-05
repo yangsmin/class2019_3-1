243ddb2e2f8628960a3dbbc49a08966e7c870ea5
yangsmin <0196484613@naver.com>
2019-09-05 16:32:00 +0900

    eclipse

--- myWorkSpace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
+++ myWorkSpace/uml/src/main/java/kr/ac/daelim/uml/Marine.java
@@ -1,21 +1,21 @@
 package kr.ac.daelim.uml;
 
-public class Marine {
+public class Marine extends Unit {
 	public int AttackPower;
 	
 	public void move()
 	{
-		System.out.println("유닛이 이동한다.");
+		System.out.println("마린이 이동한다.");
 	}
 	
 	public void attack()
 	{
-		System.out.println("유닛이 공격한다.");
+		System.out.println("마린이 공격한다.");
 	}
 	
 	public void underAttack(Charactor charactor)
 	{
-		System.out.println("유닛이 공격받는다.");
+		System.out.println("마린이 공격받는다.");
 	}
 
 
--- myWorkSpace/uml/src/main/java/kr/ac/daelim/uml/Medic.java
+++ myWorkSpace/uml/src/main/java/kr/ac/daelim/uml/Medic.java
@@ -1,19 +1,19 @@
 package kr.ac.daelim.uml;
 
-public class Medic {
+public class Medic extends Unit {
 	public void move()
 	{
-		System.out.println("유닛이 이동한다.");
+		System.out.println("메딕이 이동한다.");
 	}
 	
 	public void underAttack(Charactor charactor)
 	{
-		System.out.println("유닛이 공격받는다.");
+		System.out.println("메딕이 공격받는다.");
 	}
 	
 	public void healing(Charactor charactor)
 	{
-		System.out.println("유닛을 치료한다.");
+		System.out.println("메딕이 치료한다.");
 	}
 
 }
